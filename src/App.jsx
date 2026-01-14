9c30c126622ca366253de6aa5587860f38d3d170
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2026-01-14 15:09:32 +0530

    third-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4F46E5",
+        dark: "#111827",
+        gray: "#6B7280",
+        bg: "#F9FAFB",
+      },
+    },
+  },
+  plugins: [],
+};
